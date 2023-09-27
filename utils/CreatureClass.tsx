fbf27618e70bebffa029dedc2106ec7c81264283
Pigna99 <andrea_pigna99@hotmail.it>
2023-09-27 21:24:23 +0200

    happiness check added!

--- utils/CreatureClass.tsx
+++ utils/CreatureClass.tsx
@@ -1,5 +1,5 @@
 import { Creature , Stat, State, happiness_modifier} from "./interfaces";
-import { TICK_VALUE } from './settings';
+import { TICK_DAY, TICK_VALUE } from './settings';
 
 class CreatureClass {
     private info: Creature;
@@ -43,7 +43,7 @@ class CreatureClass {
         const last_update = new Date(this.getUpdateTime());
         const new_ticks_float= ((new Date()).getTime()-last_update.getTime()) / (60000*TICK_VALUE);
         const new_ticks = Math.floor(new_ticks_float);
-        const new_update = new Date(last_update.getTime() + new_ticks*60000*TICK_VALUE);
+        const new_update = new Date(last_update.getTime() + new_ticks*60000*TICK_VALUE);//updated timer
     
         for(let i_ticks = new_ticks; i_ticks>0; --i_ticks){//foreach tick simulate!
             this.nextTick();
@@ -51,6 +51,15 @@ class CreatureClass {
         this.setUpdateTime(new_update); //update timer to the next tick
         console.log('new ticks(float): ' + new_ticks_float);
         //console.log(this.info)
+    }
+
+    private getTicksFromDate(d:Date){//number of ticks from that Date to now
+        const new_ticks_float= ((new Date()).getTime()-d.getTime()) / (60000*TICK_VALUE);
+        const new_ticks = Math.floor(new_ticks_float);
+        return new_ticks;
+    }
+    private addTicksToDate(d:Date, t:number):Date{
+        return new Date(d.getTime() + t*60000*TICK_VALUE)
     }
 
     private nextTick(){//what happens after one tick
@@ -104,7 +113,14 @@ class CreatureClass {
             1)check that 24h has passed (288 ticks) from last happiness update
             2)if is passed, update happiness
         */
+        let ticks = this.getTicksFromDate(this.info.last_happiness_update)
+        if(ticks<TICK_DAY) return;
+        if(!checkMinStat(this.info.statictics.happiness)){
+            this.info.statictics.happiness.actual--;
+        }
+        this.info.last_happiness_update = this.addTicksToDate(this.info.last_happiness_update, TICK_DAY);//add 1 day of ticks
     }
+
     feed(){
         this.info.statictics.hunger.actual = this.info.statictics.hunger.max;
         //if stamina is >30% and was sleeping, start walking
