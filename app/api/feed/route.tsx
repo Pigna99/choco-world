afe24d63a236d7c9e6835895a06fb89ac90d0a52
Pigna99 <andrea_pigna99@hotmail.it>
2023-09-29 16:19:37 +0200

    not found

--- app/api/feed/route.tsx
+++ app/api/feed/route.tsx
@@ -7,13 +7,17 @@ export async function GET(request: Request) {
     const creatureName = searchParams.get('name');
     if(creatureName === null){
         console.log('No name');
-        return NextResponse.json({error:"no name from request"})
+        return NextResponse.json({error:"no name from request"},{
+            status: 404,
+          })
     }
 
     let files = getRoutes();//get all creature names
     if(!files.includes(creatureName)){//if the name is not found, reject the request
         console.log('Not existing creature requested');
-        return NextResponse.json({error:"cannot feed a non existing creature"})
+        return NextResponse.json({error:"cannot feed a non existing creature"},{
+            status: 404,
+          })
     }
     let creature:CreatureClass;
     creature = new CreatureClass(getCreature(creatureName));
