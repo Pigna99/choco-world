b392789438a4303399bf97fd9b5b956bb97a27b6
Pigna99 <andrea_pigna99@hotmail.it>
2023-09-29 12:33:01 +0200

    Sprite and Updates from API to Frontend

--- components/box-content.tsx
+++ components/box-content.tsx
@@ -7,48 +7,104 @@ import {useState, MouseEvent, useEffect} from 'react'
 import { spritesList } from '@/utils/utilsFrontend'
 import { Creature, VisualState } from '@/utils/interfaces'
 
-let startVisualState: VisualState = 'idle';
 let startElement: spritesList = 'stand';
 
 
 
 export const Box = ()=>{
-    const [visualState, setVisualState] = useState(startVisualState);
+    const [firstUpdate, setFirstUpdate] = useState(true)
+    const [update, setUpdate] = useState(false);
+
     const [sprite, setSprite] = useState(startElement);
     const [infoText, setInfoText] = useState('info');
     const [infoBox, setInfoBox] = useState('infos');//change to a special info class/interface
-
+    let updateTimeout:ReturnType<typeof setTimeout>;
+    const clearUpdateTimeout = ()=>{
+        //console.log(updateTimeout)
+        if(updateTimeout)clearTimeout(updateTimeout);
+    }
 
     const feedCommand = async (e:MouseEvent) =>{
-        //setSprite('walk-right')
+        clearUpdateTimeout()
+        const res = await fetch(`/api/feed?name=${creatureName}`)
+        const data = await res.json()
+        if(data.update){
+            updateVisuals('eating')
+        }else{
+            updateVisuals('idle')
+            setInfoText('not hungry')
+            console.log('You have to wait more before eating again')
+        }
+        setUpdate(!update)
     }
     const petCommand = async (e:MouseEvent) =>{
-        
-        
+        clearUpdateTimeout()
+        const res = await fetch(`/api/pet?name=${creatureName}`)
+        const data = await res.json()
+        if(data.update){
+            updateVisuals('happy')
+        }else{
+            updateVisuals('idle')
+            setInfoText('try pet later')
+            console.log('You have to wait more before petting again')
+        }
+        setUpdate(!update)
     }
     const updateCommand = async() =>{
+        clearUpdateTimeout();
+        await coreUpdate();
+        setUpdate(!update)
+        //set info and infobox
+    }
+
+    const coreUpdate = async() =>{
         const res = await fetch(`/api/update?name=${creatureName}`)
         const data = await res.json()
         
         if(!data){
             console.log("ERROR! DATA NOT RECEIVED"); return;
         }
-        console.log("Data received from API:", data)
+        //console.log("Data received from API:", data)
         const creature: Creature = data.creature;
-        setVisualState(creature.state)
-        //set info and infobox
+        updateVisuals(creature.state)
+    }
+
+    const updateVisuals = (v:VisualState)=>{
+        switch (v) {
+            case 'walking':
+                let r = Math.floor(Math.random()*4)
+                const walking_sprites:spritesList[] = ['walk-bottom','walk-right','walk-left','walk-top']
+                setSprite(walking_sprites[r])//randomize walking better
+                setInfoText('walking...')
+                break;
+            case 'sleeping':
+                setSprite('sleep')
+                setInfoText('sleeping...')
+                break;
+            case 'idle':
+                setSprite('stand')
+                break;
+            case 'eating':
+                setSprite('eat')
+                setInfoText('eating!')
+                break;
+            case 'happy':
+                setSprite('happy')
+                setInfoText('happy!')
+                break;
+            default:
+                break;
+        }
     }
 
     useEffect(() => {
-        updateCommand();
-        return () => {
+        if(firstUpdate){
+            setFirstUpdate(false);
+            updateTimeout= setTimeout(updateCommand, 0);
+            return;
         }
-    }, [])
-
-    useEffect(() => {//set the sprites based on visualState
-        
-    }, [visualState])
-    
+        updateTimeout= setTimeout(updateCommand, 5000);
+    }, [update])
 
     return(
     <div className={styles.box}>
