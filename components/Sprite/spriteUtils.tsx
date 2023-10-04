055a4dadb0952c01fc6e52e94b1651bad0b0125b
Pigna99 <andrea_pigna99@hotmail.it>
2023-10-04 23:13:47 +0200

    speed up animation by 50%

--- components/Sprite/spriteUtils.tsx
+++ components/Sprite/spriteUtils.tsx
@@ -12,7 +12,7 @@ const getSprite=(sprite:spritesList)=>{
     {sprite:stand, fps:12}
     switch (sprite) {
         case 'eat':  returnValues.sprite = eat; returnValues.fps = 8 ;break;
-        case 'happy':  returnValues.sprite = happy; returnValues.fps = 8 ;break;
+        case 'happy':  returnValues.sprite = happy; returnValues.fps = 12 ;break;
         case 'sleep':  returnValues.sprite = sleep; returnValues.fps = 8 ;break;
         case 'stand':  returnValues.sprite = stand; returnValues.fps = 4 ;break;
         case 'walk-top':  returnValues.sprite = walktop; returnValues.fps = 16 ;break;
