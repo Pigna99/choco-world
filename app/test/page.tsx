d8305b7014b749b9afc95994a5ec7977fcbc1315
Pigna99 <andrea_pigna99@hotmail.it>
2023-10-04 10:45:08 +0200

    pixelated added to css

---
+++ app/test/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Head from "next/head";
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Head>
+      </Head>
+      <div>test</div>
+    </main>
+  )
+}
