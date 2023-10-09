fd20960119b2833b9a4c415a6e88d17b0c05c1c4
Pigna99 <andrea_pigna99@hotmail.it>
2023-10-09 16:12:06 +0200

    better ui

--- components/Content/NewChoco/newChoco.tsx
+++ components/Content/NewChoco/newChoco.tsx
@@ -21,14 +21,19 @@ function NewChoco({newChoco, clicks}:{clicks:number, newChoco:(name:string,color
   
   return (
     <div className={styles.container}>
-      <h5>name:</h5>
-      <Input text={text} setText={setText} id='choconame' fontSize={25}/>
-      <h5>color:</h5>
-      <ColorInput color={color} setColor={setColor} id='chococolor'/>
-      <h5>gender:</h5>
+      <div className={styles.entryContainer}>
+        <h5>name:</h5>
+        <Input text={text} setText={setText} id='choconame' fontSize={25}/>
+      </div>
+      <div className={styles.entryContainer}>
+        <h5>color:</h5>
+        <ColorInput color={color} setColor={setColor} id='chococolor'/>
+      </div>
+      
       <div className={styles.genderContainer}>
-        <SelectedItem selected={gender==='male'} onClick={(e)=>{gender!=='male'?setGender('male'):null}}><GenderIcon gender='male' dim={100}/></SelectedItem>
-        <SelectedItem selected={gender==='female'} onClick={(e)=>{gender!=='female'?setGender('female'):null}}><GenderIcon gender='female' dim={100}/></SelectedItem>
+        <h5>gender:</h5>
+        <SelectedItem selected={gender==='male'} onClick={(e)=>{gender!=='male'?setGender('male'):null}}><GenderIcon gender='male' dim={80}/></SelectedItem>
+        <SelectedItem selected={gender==='female'} onClick={(e)=>{gender!=='female'?setGender('female'):null}}><GenderIcon gender='female' dim={80}/></SelectedItem>
       </div>
       <div style={{fontSize:20, margin:10}}>Click on the egg to make it hatch!</div>
       {
