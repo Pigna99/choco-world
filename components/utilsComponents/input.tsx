cbbe0ef7b2fd98d33aee3f6a51660fd2bdcbecba
Pigna99 <andrea_pigna99@hotmail.it>
2023-10-12 11:23:22 +0200

    permit 0 chars

--- components/utilsComponents/input.tsx
+++ components/utilsComponents/input.tsx
@@ -5,7 +5,7 @@ import styles from './utils.module.css'
 const  Input = ({text, setText, id, fontSize}:{text:string, setText:React.Dispatch<React.SetStateAction<string>>, id:string, fontSize?:number})=>{
 
     const handleChange =( e:React.FormEvent<HTMLInputElement>) => {
-        const letterNumber = /^[0-9a-zA-Z]+$/;
+        const letterNumber = /^[0-9a-zA-Z]*$/;
         if(!e.currentTarget.value.match(letterNumber))return;
         setText(e.currentTarget.value);
         //console.log('value is:', e.currentTarget.value);
@@ -19,7 +19,6 @@ const  Input = ({text, setText, id, fontSize}:{text:string, setText:React.Dispat
         name={id}
         onChange={handleChange}
         value={text}
-        pattern='[a-zA-Z0-9-]+'
         style={{fontSize:fontSize ? fontSize : 15}}
       />)
 }
