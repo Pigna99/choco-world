e45d3fd41738da0c81c53def2b56734994f6378a
Pigna <andrea_pigna99@hotmail.it>
2023-10-19 11:57:22 +0200

    added last interaction time

--- components/Content/Commands/commands.tsx
+++ components/Content/Commands/commands.tsx
@@ -1,14 +1,59 @@
 
 import { Button } from '@/components/utilsComponents/buttons'
 import styles from './commands.module.css'
-import { MouseEventHandler } from "react"
+import { MouseEventHandler, useEffect, useState } from "react"
 import { useFetchContext } from '@/components/context/fetchcontext'
 
 export const Commands = ({feedCommand, petCommand, block, info}:{feedCommand:MouseEventHandler, petCommand:MouseEventHandler, block:boolean, info:string})=>{
     const {creatureInfo} = useFetchContext()
+    const [isInteraction, setIsInteraction] = useState(false);
+    const [lastInteractionTime, setLastInteractionTime] = useState('>1 minute ago');
+
+    useEffect(() => {
+        updateInteractionText()
+        setTimeout(()=>setIsInteraction(!isInteraction),30000)
+    }, [isInteraction])
+
+    useEffect(()=>{
+        updateInteractionText()
+    },[creatureInfo])
+
+    const updateInteractionText= ()=>{
+        const earlier_interaction = creatureInfo.last_time_feed>creatureInfo.last_time_pet ? creatureInfo.last_time_feed : creatureInfo.last_time_pet
+        setLastInteractionTime(calculateLastInteractionTime(new Date(earlier_interaction)))
+    }
+    const calculateLastInteractionTime = (last:Date):string=>{
+        const time = Date.now() - last.getTime();
+        let number = 0;
+        if(time>86400000){
+            number = Math.floor(time/86400000)
+            if(number===1){
+                return `1 day ago`
+            }
+            return `${number} days ago`
+        }
+        if(time>3600000){
+            number = Math.floor(time/3600000)
+            if(number===1){
+                return `1 hour ago`
+            }
+            return `${number} hours ago`
+        }
+        if(time>60000){
+            number = Math.floor(time/60000)
+            if(number===1){
+                return `1 minute ago`
+            }
+            return `${number} minutes ago`
+        }
+
+        return `>1 minute ago`
+    }
+    
     return(
         <div className={styles.container}>
             <div className={styles.infoText}>{`${creatureInfo.name} ${info}`}</div>
+            <div className={styles.interactionText}>last interaction: {lastInteractionTime}</div>
             <div className={styles.buttonContainer}>
                 <Button name={'Feed'} clickEvent={feedCommand} blocked={block}/>
                 <Button name={'Pet'} clickEvent={petCommand} blocked={block}/>
